03b4bbcea5260794bedb42138beedd76850749e6
egor <egor.no.login@gmail.com>
2026-09-02 22:23:20 +0300

    init

---
+++ src/main/java/dasein/sem1pks/Sem1pksApplication.java
@@ -0,0 +1,13 @@
+package dasein.sem1pks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sem1pksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sem1pksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dasein/sem1pks/Sem1pksApplicationTests.java
@@ -0,0 +1,13 @@
+package dasein.sem1pks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem1pksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
