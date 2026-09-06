bd9df8e084450881f809a49ea437088a76aaa7a2
egor <egor.no.login@gmail.com>
2026-09-06 18:09:12 +0300

    add account request dto

---
+++ src/main/java/dasein/sem1pks/dto/request/AccountLoginRequest.java
@@ -0,0 +1,26 @@
+package dasein.sem1pks.dto.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountLoginRequest {
+
+    @NotBlank(message = "Email обязателен")
+    @Email(message = "Некорректный формат email")
+    private String email;
+
+    @NotBlank(message = "Пароль обязателен")
+    @Size(min = 6, message = "Пароль должен содержать минимум 6 символов")
+    private String password;
+}
---
+++ src/main/java/dasein/sem1pks/dto/request/AccountRegisterRequest.java
@@ -0,0 +1,24 @@
+package dasein.sem1pks.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountRegisterRequest {
+
+    @NotBlank(message = "Email обязателен")
+    @Email(message = "Некорректный формат email")
+    private String email;
+
+    @NotBlank(message = "Пароль обязателен")
+    @Size(min = 6, max = 30, message = "Пароль должен быть от 6 до 30 символов")
+    private String password;
+}
