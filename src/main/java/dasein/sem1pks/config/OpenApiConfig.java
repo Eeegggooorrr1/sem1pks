3392667d0b249fc9afadba5805451e5a66cda77b
Аникеев Егор <anikeev.e.a@edu.mirea.ru>
2026-09-14 21:29:57 +0300

    refactor swagger ui

--- src/main/java/dasein/sem1pks/config/OpenApiConfig.java
+++ src/main/java/dasein/sem1pks/config/OpenApiConfig.java
@@ -15,9 +15,9 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Marketplace API")
+                        .title("API")
                         .version("1.0")
-                        .description("REST API for marketplace application"))
+                        .description("невыносимая легкость бытия"))
                 .components(new Components()
                         .addSecuritySchemes(
                                 "bearerAuth",
